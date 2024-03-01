cce93dfa3f3ba4a1a0b7fa0b1b3d5f293b4d4aa6
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-03-01 10:49:37 -0800

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.34%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        string result = "";
         int count1 = 0;
         int count0 = 0;
         for(char c : s){
             if(c == '1') count1++;
             else count0++;
         }
-        if(count1 > 1){
-            while(count1 - 1 != 0){
-                result += '1';
-                count1--;
-            }
-        }
-        while(count0){
-            result += '0';
-            count0--;
-        }
-        result += '1';
+        string result = string(count1 - 1, '1') + string(count0, '0') + '1';
         return result;
     }
 };
