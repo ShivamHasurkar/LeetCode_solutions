65b1674779b9fdb57356630125ee63f6269bae9f
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-03-06 11:15:48 -0800

    Time: 4 ms (48.38%) | Memory: 7.9 MB (43.15%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int x = 0;
+        char x = 0;
         for(char c : s) x = x ^ c;
 
         for(char c: t) x = x ^ c;
