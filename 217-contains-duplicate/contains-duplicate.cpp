2d51b19d4015d39229315d0198cc179eea677d8c
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-03-07 17:55:51 -0800

    Time: 86 ms (80.90%) | Memory: 73.3 MB (24.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -5,7 +5,9 @@ public:
         unordered_set<int> set(n);
         
         for(int i=0;i<n;i++){
-            if(!set.insert(nums[i]).second) return true;
+            if(set.contains(nums[i]))
+                return true;
+            set.insert(nums[i]);
         }
         return false;
     }
