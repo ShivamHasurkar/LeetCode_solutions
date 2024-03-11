9744fb45ec823d2ca7c56151f0e2d58c2a7f1a98
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-03-11 15:49:57 -0700

    Time: 3 ms (71.84%) | Memory: 12 MB (40.64%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -11,10 +11,12 @@ public:
 
             if(nums[mid] == target) return mid;
             else if (nums[mid] < target){
-                left = mid + 1;
+                if(nums[mid+1] > target) return mid + 1;
+                else left = mid + 1;
             }
             else {
-                right = mid - 1;
+                if(nums[mid - 1] < target) return mid;
+                right = mid;
             }
         }
         return left;
