fb0f562000b9304cb00b69b8d3bd58e8ef934240
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-03-19 01:18:33 -0700

    Time: 3 ms (60.75%) | Memory: 8.6 MB (55.68%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,6 +1,9 @@
 class Solution{
     public:
-       string gcdOfStrings(string str1, string str2){
-        return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(str1.size(), str2.size())) : "";
-       }
+    string gcdOfStrings(string str1, string str2){
+    return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(str1.size(), str2.size())) : "";
+    }
+    int gcd(int a, int b){
+    return b == 0 ? a : gcd(b, a % b);
+    }
 };
