f405e934fe4710510c493f1384a93b6643dcc676
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-10-04 00:01:33 -0700

    Time: 38 ms (20.19%) | Memory: 31.9 MB (21.00%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    void backtrack(vector<int>& candidates, int target, int currSum, vector<int>& curr, vector<vector<int>>& ans){
+    void backtrack(vector<int> candidates, int target, int currSum, vector<int>& curr, vector<vector<int>>& ans){
         if(currSum == target){
             ans.push_back(curr);
         }
         if(currSum > target) return;
 
-        for(int i=0; i<candidates.size(); i++){
-            if(curr.size() > 0 && candidates[i] < curr.back()) continue; 
+        for(int i=0;i<candidates.size();i++){
+            if(curr.size() > 0 && candidates[i] < curr.back()) continue;
             curr.push_back(candidates[i]);
             currSum += candidates[i];
             backtrack(candidates, target, currSum, curr, ans);
