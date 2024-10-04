f87953bc6c15d0b5be49cdb9dda5f41172994832
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-10-04 10:34:24 -0700

    Time: 3 ms (38.00%) | Memory: 8.5 MB (61.80%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void backtrack(string digits, string& curr, vector<string>& ans, vector<string> mapping, int idx){
-        if(idx == digits.size()){
+    void backtrack(vector<string>& candidates, string digits, int idx, string& curr, vector<string>& ans){
+        if(digits.size() == idx){
             ans.push_back(curr);
             return;
         }
-        string str = mapping[digits[idx] - '0'];
-        for(int j=0;j<str.size(); j++){
-            curr += str[j];
-            backtrack(digits, curr, ans, mapping, idx+1);
+        string str = candidates[digits[idx] - '0'];
+        for(int i=0;i<str.size();i++){
+            curr += str[i];
+            backtrack(candidates, digits, idx+1, curr, ans);
             curr.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
         if(digits.size() == 0) return {};
-        vector<string> mapping = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> candidates = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}; 
         vector<string> ans;
         string curr = "";
-        backtrack(digits, curr, ans, mapping, 0);
+        backtrack(candidates, digits, 0, curr, ans);
         return ans;
     }
 };
-
