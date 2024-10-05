79303d1e872de999c632bfd091a6bccc5725cded
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-10-04 22:58:55 -0700

    Time: 0 ms (100.00%) | Memory: 16.1 MB (16.96%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void backtrack(int n, int i, int j, string curr, vector<string>& ans){
+        if(i==j && j==n){
+            ans.push_back(curr);
+            return;
+        }
+        if(i<n){
+            curr += '(';
+            backtrack(n, i+1, j, curr, ans);
+            curr.pop_back();
+        }
+        if(j<i){
+            curr += ')';
+            backtrack(n, i, j+1, curr, ans);
+            curr.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string curr = "";
+        backtrack(n, 0, 0, curr, ans);
+        return ans;
+    }
+};
