9a310c157641871a8047e2050e1229ae7ff4ad03
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2024-10-07 15:24:45 -0700

    Time: 9 ms (22.25%) | Memory: 8.9 MB (53.89%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(iswalnum(s[i])){
+                if(iswalnum(s[j])){
+                    if(tolower(s[i++]) != tolower(s[j--])) return false;
+                }
+                else j--;
+            }
+            else i++;
+        }
+        return true;
+    }
+};
